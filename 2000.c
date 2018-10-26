18e8335f65308cc2aaf8f5caf5508e89bd0e73f4
DanielKubanek <danielkubanek@gmail.com>
2018-10-26 09:19:42 +0200

    wyswietlam liczby 0-2000 niepodzielne przez 2 3 5 7
    wykluczylem liczby parzyste

--- 2000.c
+++ 2000.c
@@ -3,19 +3,26 @@
 
 int main() {
     int licznik_petli = 0;
+    int x;
+    {
+        printf("podaj liczbe do sprawdzenia: \n");
+        scanf("%i", &x);
 
-        for (int x =1000; x>=0; --x) {
-            licznik_petli++;
-            if(x%2 && x%3 && x%5 && x%7){
+        if(!(x%2))?// sprawdzenie czy liczba jest liczba parzysta
+        x-=1; {    // jesli jest parzysta zmniejszamliczbe o 1
+        for (&x; x >= 0; x-=2) {
+
+            if (x % 2 && x % 3 && x % 5 && x % 7) {
 
                 printf("%d\n ", x);
 
+                licznik_petli++;
 
-            
-            break;}
-        }
+                break;
+            }
+        }}
 
-
+    }
 printf("\n");
 printf("liczba petli wyniosła: %d\n", licznik_petli);
 
