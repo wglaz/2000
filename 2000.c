ba3484e20aeb7495ade52cde11c4995661e76720
DanielKubanek <danielkubanek@gmail.com>
2018-10-26 09:48:04 +0200

    drobne korekty w dzialaniu zliczania petli

--- 2000.c
+++ 2000.c
@@ -8,17 +8,17 @@ int main() {
         printf("podaj liczbe do sprawdzenia: \n");
         scanf("%i", &x);
 
-        if(!(x%2))?// sprawdzenie czy liczba jest liczba parzysta
+        if(!(x%2))// sprawdzenie czy liczba jest liczba parzysta
         x-=1; {    // jesli jest parzysta zmniejszamliczbe o 1
-        for (&x; x >= 0; x-=2) {
+        for (&x; x >= 0; x-=2){
 
-            if (x % 2 && x % 3 && x % 5 && x % 7) {
+            if ( x % 3 && x % 5 && x % 7) break;{
 
                 printf("%d\n ", x);
+                ++licznik_petli;
 
-                licznik_petli++;
 
-                break;
+
             }
         }}
 
