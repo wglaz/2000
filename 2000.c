e09954560dd81482fcb60c4717e62a7f38c6a028
DanielKubanek <danielkubanek@gmail.com>
2018-10-26 10:26:41 +0200

    dodanie słowa "continue"

--- 2000.c
+++ 2000.c
@@ -5,22 +5,26 @@ int main() {
     int licznik_petli = 0;
     int x;
     {
-        printf("podaj liczbe do sprawdzenia: \n");
+       printf("podaj liczbe do sprawdzenia: \n");
         scanf("%i", &x);
 
         if(!(x%2))// sprawdzenie czy liczba jest liczba parzysta
-        x-=1; {    // jesli jest parzysta zmniejszamliczbe o 1
-        for (&x; x >= 0; x-=2){
+        x-=1;    // jesli jest parzysta zmniejszamliczbe o 1
+        for (&x; x >= 0; x-=2) {
+            ++licznik_petli;
 
-            if ( x % 3 && x % 5 && x % 7) break;{
-
-                printf("%d\n ", x);
-                ++licznik_petli;
-
+            if (!(x % 3))continue;
+            if (!(x % 5))continue;
+            if (!(x % 7))continue;
 
 
-            }
-        }}
+            printf("%d\n ", x);
+
+        }
+
+
+
+
 
     }
 printf("\n");
